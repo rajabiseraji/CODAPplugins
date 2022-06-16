5b8a0540123006be0eb76f0fe9e4d16f9ebc713c
Mohammad Rajabi <rajabiseraji@outlook.com>
2022-06-16 16:41:10 -0700

    fix(deleteComponent): fixes what happens when we delete a comp

--- dset/estim8/codapNotificationHandler.js
+++ dset/estim8/codapNotificationHandler.js
@@ -18,26 +18,30 @@ export const codapNotificationHandler = {
         console.log(imsg);
         
         if(imsg.values.type !== "graph")
-            return;
-    
-            // as the next step, just find it using the codap interface
-            
+        return;
+        
+        // as the next step, just find it using the codap interface
+        
         var id = imsg.values.id;
         findComponent(id, addToComponentList);
-            
+        
     },
     
     componentDeleteHandler: function(imsg) {
         console.log("a component just got deleted: ");
         console.log(imsg);
         
-        if(imsg.values.type !== "graph")
+        if(imsg.values.type !== "DG.GraphView") // for some reason the name in delete differs from the name in create and change
         return;
         
         var id = imsg.values.id;
         removeComponentFromList(id);
-    },
 
+
+        console.log("here's the component list after the fact");
+        console.log(componentList);
+    },
+    
 }
 
 
@@ -57,13 +61,16 @@ async function findComponent(CODAPcomponentID, callbackfn) {
 }
 
 function addToComponentList(resultObjectFromCodap, CODAPcomponentID) {
-    component =  {
+    var component =  {
         id: CODAPcomponentID, 
-        values: result.values
+        values: resultObjectFromCodap.values
     };
     var exists = componentList.some((el) => el.id === CODAPcomponentID);
     if (!exists)
         componentList.push(component);
+
+    console.log("here's the component list after the fact");
+    console.log(componentList);
 }
 
 function removeComponentFromList(CODAPcomponentID) {
@@ -76,6 +83,9 @@ function changeComponentListItem(resultObjectFromCodap, CODAPcomponentID) {
     var foundIndex = componentList.findIndex((el) => el.id === CODAPcomponentID);
     if(foundIndex !== -1) 
         componentList[foundIndex].values = resultObjectFromCodap.values;
+
+    console.log("here's the component list after the fact");
+    console.log(componentList);
 }
 
 
